181bb33b0eaa137d62d614d8c35ad79a87468703
Jeff Lieb <blockbattle@gmail.com>
2022-07-18 22:57:15 -0400

    Initial commit

---
+++ test/whatever.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+const Phone = require('../src/phone.js')
+
+describe("Phone", () => {
+    it("reports when it dials a 5", () => {
+        expect(5).to.equal(5)
+    })
+})
